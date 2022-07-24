47edda71f6218800fa835497897b00c1ce76ec9f
Ctyring <173479693@qq.com>
2022-07-25 00:01:47 +0800

    加载yaml

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,14 +1,56 @@
 #include "sylar/config.h"
 #include "sylar/log.h"
+#include <yaml-cpp/yaml.h>
 
 sylar::ConfigVar<int>::ptr g_int_value_config = sylar::Config::Lookup("system.prot", (int)8080, "system port");
 
 sylar::ConfigVar<float>::ptr g_float_value_config =
     sylar::Config::Lookup("system.value", (float)10.2f, "system value");
 
+void print_yaml(const YAML::Node &node, int level)
+{
+    if (node.IsScalar())
+    {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                                         << node.Scalar() << " - " << node.Type() << " - " << level;
+    }
+    else if (node.IsNull())
+    {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                                         << "NULL - " << node.Type() << " - " << level;
+    }
+    else if (node.IsMap())
+    {
+        for (auto it = node.begin();
+             it != node.end(); ++it)
+        {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                                             << it->first << " - " << it->second.Type() << " - " << level;
+            print_yaml(it->second, level + 1);
+        }
+    }
+    else if (node.IsSequence())
+    {
+        for (size_t i = 0; i < node.size(); ++i)
+        {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                                             << i << " - " << node[i].Type() << " - " << level;
+            print_yaml(node[i], level + 1);
+        }
+    }
+}
+
+void test_yaml()
+{
+    YAML::Node root = YAML::LoadFile("/root/cty/sylar/bin/conf/log.yml");
+    print_yaml(root, 0);
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root.Scalar();
+}
+
 int main(int argc, char const *arfv[])
 {
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_float_value_config->toString();
+    test_yaml();
     return 0;
 }
